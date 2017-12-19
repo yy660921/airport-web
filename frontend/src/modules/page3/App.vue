a97dd9ba92d04b312781714583e525b1c5edcd38
guosk <guosk@act.buaa.edu.cn>
2017-12-19 17:34:31 +0800

    添加page3布局

--- frontend/src/modules/page3/App.vue
+++ frontend/src/modules/page3/App.vue
@@ -1,29 +1,276 @@
 <template>
-  <div id="app">
-    <v-header></v-header>
-    <!--通过vue-router控制主内容区域变换-->
-    <router-view></router-view>
+  <div id="app" class="real-body">
+    <div class="con-left">
+      <div class="con-box">
+        <Echarts theme="ring" :option="l_t_option" className="chart" ></Echarts>
+      </div>
+      <div class="con-box">
+        <Echarts theme="ring" :option="l_t_option" className="chart" ></Echarts>
+      </div>
+    </div>
+    <div class="con-right">
+      <h3>热点新闻</h3>
+      <ul>
+        <li class="list active">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘南开区环保招聘南开区环保招聘南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+          <p class="news-detail">河北区 86295673 铁东路街社区卫生服务中心 河北区南口路186号 每周一、二、四、五上午 86662997 望海楼街社区卫生服务中心 河北区昆纬路72号 每周一至周五上午 26219502 新开河街社区卫生服务中心 河北区古北道10号 86651424 月牙河街社区卫生服务中心 河北区靖江路雅砻江道2号 26025637 江都路街社区卫生服务中心 河北区金山道2号</p>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+        <li class="list">
+          <a href="#" class="link"><i class="news-icon fa fa-newspaper-o"></i><span>[社会焦点]</span>天津欣国环环保科技有限公司最新天津市/南开区环保招聘</a>
+          <i class="news-arrow fa fa-angle-double-down"></i>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue'
+  import 'components/charts/theme/Ring.js'
+  import Graphic from 'echarts/lib/util/graphic'
+  import Echarts from 'vue-echarts-v3/src/full.js'
 
-import Vue from 'vue'
+  /* Globally bus for Non Parent-Child Communication */
+  window.bus = new Vue();
 
-// Require custom echarts theme.
-import 'components/charts/theme/Ring.js'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        l_t_option: {
+          title: {
+            text: '风险走势',
+            left: 'center',
+            textStyle: {
+              color: '#b5eaff'
+            }
+          },
+          tooltip: {},
+          xAxis: {
+            data: ['12-01', '12-02', '12-03', '12-04', '12-05', '12-06', '12-07', '12-08'],
+            // 坐标轴轴线
+            axisLine: {
+              lineStyle: {
+                width: 2,
+                color: '#1a41ac'
+              }
+            },
+            // 坐标轴刻度标签
+            axisLabel: {
+              color: '#fff',
+              fontSize: 14
+            },
+            boundaryGap: false,
+            // 坐标轴刻度
+            axisTick: {
 
-/* Globally bus for Non Parent-Child Communication */
-window.bus = new Vue();
-
-export default {
-  name: 'app',
-  components: {
+            }
+          },
+          yAxis: {
+            // 坐标轴轴线
+            axisLine: {
+              lineStyle: {
+                width: 2,
+                // type: 'solid',
+                color: '#1a41ac'
+              }
+            },
+            // 坐标轴刻度标签
+            axisLabel: {
+              color: '#fff',
+              fontSize: 14
+            },
+            splitLine: {
+              lineStyle: {
+                color: '#1a41ac'
+              }
+            }
+          },
+          grid: {
+            top: 50,
+            left: 30,
+            right: 20,
+            bottom: 25
+          },
+          series: [
+            {
+              name: '选项B',
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: 'rgba(252, 143, 47, .8)'
+                }
+              },
+              areaStyle: {
+                normal: {
+                  color: new Graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgba(252, 143, 47, .9)'
+                  }, {
+                    offset: 1,
+                    color: 'rgba(252, 143, 47, .3)'
+                  }])
+                }
+              },
+              data: [20, 10, 20, 30, 35, 26, 36, 30]
+            },
+            {
+              name: '选项A',
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: 'rgba(41, 202, 255, .8)'
+                }
+              },
+              areaStyle: {
+                normal: {
+                  color: new Graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgba(59, 159, 237, .9)'
+                  }, {
+                    offset: 1,
+                    color: 'rgba(59, 159, 237, .3)'
+                  }])
+                }
+              },
+              data: [10, 20, 30, 15, 12, 20, 38, 20]
+            }
+          ]
+        }
+      }
+    },
+    components: {
+      Echarts
+    }
   }
-}
 </script>
 
 <style lang="sass">
-  @import "~assets/sass/base"
   @import "~assets/sass/common"
+
+  .con-left
+    float: left
+    width: 48%
+    height: 100%
+    .con-box
+      height: 48%
+      padding: .7rem 1rem .8rem
+      background-image: url("~assets/images/page3-left-bg.png")
+      background-size: 100% 100%
+      &:first-child
+        margin-bottom: 4%
+  .con-right
+    float: right
+    width: 49%
+    height: 100%
+    padding: 1rem 1rem .8rem
+    background-image: url("~assets/images/page3-right-bg.png")
+    background-size: 100% 100%
+    overflow-y: hidden
+    h3
+      color: #b5eaff
+      font-size: 18px
+      font-weight: bold
+      text-align: center
+      margin-bottom: 0
+    ul
+      margin-top: 1rem
+      padding: 0rem 1rem
+      height: calc(100% - 50px)
+      overflow-y: auto
+      li.list
+        position: relative
+        background: rgba(6, 22, 58, .5)
+        border: 1px solid #2052dd
+        padding: .5rem 1rem
+        margin-bottom: 1.1rem
+        .news-detail
+          margin-bottom: 0
+          color: #eee
+          display: none
+        &.active
+          background: rgba(32, 82, 211, .5)
+          .link
+            color: #fff
+            padding-bottom: .4rem
+            margin-bottom: .4rem
+            border-bottom: 1px dashed #ccc
+          .news-arrow
+            &:before
+              content: "\F102"
+          .news-detail
+            display: block
+        .link
+          display: block
+          color: #b5eaff
+          padding-right: 1rem
+          white-space: nowrap
+          text-overflow: ellipsis
+          overflow: hidden
+          .news-icon
+            vertical-align: middle
+            margin-right: .4rem
+          span
+            margin-right: .2rem
+        .news-arrow
+          display: inline-block
+          position: absolute
+          width: 1.5rem
+          height: 1.5rem
+          line-height: 1.5rem
+          text-align: center
+          right: 1rem
+          font-size: 24px
+          color: #b5eaff
+          top: .35rem
+          display: block
+          cursor: pointer
+        
+
+
 </style>
