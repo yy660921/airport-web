996472a45789632b2c1658794cfb4bc4f197c60f
itaizy <itaizy@163.com>
2017-12-20 15:19:32 +0800

    个人信息+入境时间分布

--- frontend/src/modules/page2/App.vue
+++ frontend/src/modules/page2/App.vue
@@ -16,17 +16,18 @@
         </div>
         <div class="circle circle-1">
           <!-- <img src="~assets/images/circle-1.png" alt=""> -->
-          <span>77</span>
+          <span>{{ riskIndex }}</span>
         </div>
       </div>
       <div class="info-detail">
-        <p>姓名：<span>张三</span></p>
-        <p>国籍：<span>中国</span></p>
-        <p>出生日期：<span>1988-01-01</span></p>
-        <p>护照号：<span>XXXXXXXXXX</span></p>
-        <p>最近一次航班出发地：<span>北京</span></p>
-        <p>最近一次航班目的地：<span>铜仁</span></p>
-        <p>入境时间：<span>2017-12-13 12:34:56</span></p>
+        <p>姓名：<span>{{ username }}</span></p>
+        <p>性别：<span>{{ sex }}</span></p>
+        <p>国籍：<span>{{ country }}</span></p>
+        <p>出生日期：<span>{{ birth }}</span></p>
+        <p>护照号：<span>{{ passportID }}</span></p>
+        <p>最近一次航班出发地：<span>{{ departure }}</span></p>
+        <p>最近一次航班目的地：<span>{{ destination }}</span></p>
+        <p>入境时间：<span>{{ intime }}</span></p>
       </div>
     </div>
     <div class="guest-charts clearfix">
@@ -57,18 +58,29 @@ export default {
   name: 'app',
   data () {
     return {
+      username: '张三',
+      sex: '男',
+      country: '中国',
+      birth: '1990-01-01',
+      passportID: 'J94875326',
+      departure: '巴黎',
+      destination: '铜仁',
+      intime: '2017-12-16',
+      riskIndex: '99',
       left_option: {
+        animation: true,
         title: {
-          text: '入境时间分析',
+          text: '入境时间分布',
           left: 'center',
           textStyle: {
             color: '#b5eaff'
           }
         },
-        tooltip: {},
+        tooltip: {
+          trigger: 'axis',
+        },
         xAxis: {
-          data: ['12-01', '12-02', '12-03', '12-04', '12-05', '12-06', '12-07', '12-08'],
-          // 坐标轴轴线
+          type: 'category',
           axisLine: {
             lineStyle: {
               width: 2,
@@ -80,14 +92,24 @@ export default {
             color: '#fff',
             fontSize: 14
           },
+          axisTick: {
+            show: false
+          },
+          splitLine: {
+            lineStyle: {
+              type: 'solid',
+              color: '#cfc3bd'
+            }
+          },
           boundaryGap: false,
+          data: ['2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12']
         },
         yAxis: {
-          // 坐标轴轴线
+          type: 'value',
+          position: 'top',
           axisLine: {
             lineStyle: {
               width: 2,
-              // type: 'solid',
               color: '#1a41ac'
             }
           },
@@ -103,53 +125,52 @@ export default {
           }
         },
         grid: {
-          top: 50,
-          left: 30,
+          top: 60,
+          left: 60,
           right: 20,
-          bottom: 25
+          bottom: 10
         },
         series: [
           {
-            name: '选项B',
-            type: 'line',
+            name: '',
+            type: 'bar',
+            barWidth: 14,
+            barGap: '-100%',
             itemStyle: {
               normal: {
-                color: 'rgba(252, 143, 47, .8)'
+                color: 'rgba(64, 42, 31, .8)',
               }
             },
-            areaStyle: {
-              normal: {
-                color: new Graphic.LinearGradient(0, 0, 0, 1, [{
-                  offset: 0,
-                  color: 'rgba(252, 143, 47, .9)'
-                }, {
-                  offset: 1,
-                  color: 'rgba(252, 143, 47, .3)'
-                }])
-              }
-            },
-            data: [20, 10, 20, 30, 35, 26, 36, 30]
+            // TODO: 动态获取坐标轴的最大值
+            // data: data.map(function() {return xMax})
+            data: [10,10,10,10,10,10,10]
           },
           {
-            name: '选项A',
-            type: 'line',
-            itemStyle: {
+            name: '设备查验次数',
+            type: 'bar',
+            barWidth: 14,
+            label: {
               normal: {
-                color: 'rgba(41, 202, 255, .8)'
+                textStyle: {
+                  color: '#682d19',
+                },
+                position: 'right',
+                show: false,
+                formatter: '{b}'
               }
             },
-            areaStyle: {
+            itemStyle: {
               normal: {
-                color: new Graphic.LinearGradient(0, 0, 0, 1, [{
+                color: new Graphic.LinearGradient(1, 0, 0, 0, [{
                   offset: 0,
-                  color: 'rgba(59, 159, 237, .9)'
+                  color: 'rgba(255, 162, 82, 1)'
                 }, {
                   offset: 1,
-                  color: 'rgba(59, 159, 237, .3)'
+                  color: 'rgba(241, 90, 34, 1)'
                 }])
               }
             },
-            data: [10, 20, 30, 15, 12, 20, 38, 20]
+            data: [7,6,5,4,3,2,1]
           }
         ]
       },
@@ -346,6 +367,48 @@ export default {
         ]
       }
     }
+  },
+  created () {
+    this.updateData();
+  },
+  methods: {
+    updateData: function () {
+      axios.get('/api/getTourist', {}).then(response => {
+          if (response.data.length > 0) {
+            var res = response.data;
+            for (var i = 0; i < res.length; i++) {
+              var person = res[i]
+              this.username = person.warningTourist_name;
+              this.sex = person.warningTourist_sex;
+              this.country = person.warningTourist_country;
+              this.birth = person.warningTourist_birthday;
+              this.passportID = person.warningTourist_passport;
+              this.departure = person.warningTourist_departure;
+              this.destination = person.warningTourist_destination;
+              this.intime = person.warningTourist_time;
+              this.riskIndex = person.warningTourist_riskIndex;
+              this.update_left_option(person.warningTourist_historyTime);
+              break;
+            }
+          }
+        });
+    },
+    update_left_option: function (data) {
+      for (var p in data) {
+        var pdata = data[p]
+        var xdata = []
+        var linedata = []
+        for (var tp in pdata) {
+          xdata.push(tp)
+          linedata.push(pdata[tp])
+        }
+        this.left_option.xAxis.data = xdata;
+        this.left_option.series[1].data = linedata;
+        var maxd = _.max(linedata);
+        this.left_option.series[0].data = _.map(linedata, (obj, idx) => { return maxd })
+        break;
+      }
+    },
   },
   components: {
     Echarts
