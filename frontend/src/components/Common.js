12f1b06ae9730cbd92871f357070f077b59b2db4
HE, Tao <sighingnow@gmail.com>
2018-02-08 12:51:42 +0800

    Configs.
    Signed-off-by: HE, Tao <sighingnow@gmail.com>

--- frontend/src/components/Common.js
+++ frontend/src/components/Common.js
@@ -1,7 +1,12 @@
 export default {
-  addr: 'http://localhost:10010/',
-  page0: 'page1',
-  page1: 'customs/index.html',
-  page2: 'customs/traveler.html',
-  page3: 'customs/publicOpinion.html'
+  // addr: 'http://localhost:10010/',
+  // page0: 'page1',
+  // page1: 'customs/index.html',
+  // page2: 'customs/traveler.html',
+  // page3: 'customs/publicOpinion.html'
+  addr: 'http://218.106.244.92/',
+  page0: 'index.html',
+  page1: 'index.html',
+  page2: 'traveler.html',
+  page3: 'publicOpinion.html'
 };
